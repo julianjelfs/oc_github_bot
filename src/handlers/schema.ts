2e36f4f33dab8b540d0ed94f0c726330f1ccf73c
Julian Jelfs <julian.jelfs@gmail.com>
2025-03-19 18:56:09 +0000

    add dummy command

--- src/handlers/schema.ts
+++ src/handlers/schema.ts
@@ -5,7 +5,20 @@ export default function schema(_: Request, res: Response) {
   res.status(200).json({
     description:
       "This bot will act as a webhook to accept a PR notification from github and post it to a chat.",
-    commands: [],
+    commands: [
+      {
+        name: "dummy",
+        default_role: "Owner",
+        direct_messages: false,
+        description: "This is just to get round a bug",
+        permissions: Permissions.encodePermissions({
+          chat: [],
+          community: [],
+          message: ["Text"],
+        }),
+        params: [],
+      },
+    ],
     autonomous_config: {
       sync_api_key: true,
       permissions: Permissions.encodePermissions({
