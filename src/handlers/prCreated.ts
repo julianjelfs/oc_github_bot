e2f1991509c35bdb049dab4daddb0358aff8f2d5
Julian Jelfs <julian.jelfs@gmail.com>
2025-03-20 09:16:45 +0000

    format message a bit better

--- src/handlers/prCreated.ts
+++ src/handlers/prCreated.ts
@@ -11,17 +11,22 @@ export default async function prCreated(req: Request, res: Response) {
     return;
   }
 
-  const { title, url, author } = req.body as {
+  const { title, url, author, repo } = req.body as {
     title: string;
     url: string;
     author: string;
+    repo: string;
   };
 
   const client = req.botClient;
 
   const msg = (
     await client.createTextMessage(
-      `[PR "${title}" create by ${author}](${url})`
+      `
+      ## A PR has been created in the ${repo} repository by ${author}
+      
+      [${title}](${url})
+      `
     )
   ).setFinalised(true);
 
