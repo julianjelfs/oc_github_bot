3da02ad5d2e3d6dbb14faf5977e1581502855457
Julian Jelfs <julian.jelfs@gmail.com>
2025-03-21 18:48:54 +0000

    update the schema to remove dummy command

--- src/handlers/schema.ts
+++ src/handlers/schema.ts
@@ -5,20 +5,7 @@ export default function schema(_: Request, res: Response) {
   res.status(200).json({
     description:
       "This bot will act as a webhook to accept a PR notification from github and post it to a chat.",
-    commands: [
-      {
-        name: "dummy",
-        default_role: "Owner",
-        direct_messages: false,
-        description: "This is just to get round a bug",
-        permissions: Permissions.encodePermissions({
-          chat: [],
-          community: [],
-          message: ["Text"],
-        }),
-        params: [],
-      },
-    ],
+    commands: [],
     autonomous_config: {
       sync_api_key: true,
       permissions: Permissions.encodePermissions({
